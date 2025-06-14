67302a162c189e83308f9ad6174252781a0966ba
Piotr Ladkowski <piolad2003@gmail.com>
2025-06-14 12:20:48 +0200

    WIP guessing game

--- guessing-game/src/main.rs
+++ guessing-game/src/main.rs
@@ -1,92 +1,142 @@
+use std::io;
+use std::collections::HashMap;
+use rand::Rng;
 use crossterm::{
     event::{self, read, Event, KeyCode, KeyEvent},
     terminal::{enable_raw_mode, disable_raw_mode},
 };
 
-use std::io;
 
 
-#[derive(Debug)]
+#[derive(Debug, Hash, Eq, PartialEq)]
 enum InputResult {
     Char(char),
+    Up,
+    Down,
+    Left,
+    Right,
     Exit,
     Help,
     InvalidKey,
 }
 
+#[derive(Debug)]
+enum GameState {
+    Start,
+    Playing,
+    Ended,
+}
+
+fn main() {
+    let mut default_press_handler: HashMap<InputResult, Box<dyn Fn()> > = HashMap::new();
+    let mut gamestate = GameState::Start;
+    setup(&mut default_press_handler );
+
+    // if(gamestate == GameState::Start) {
+        // print_welcome_message();
+    // }
+
+    
+    let mut result: u32 = rand::thread_rng().r#gen::<u32>();
+    
+    println!("Hello, lets play a guessing game!");
+    println!("-> Press Enter to start");
+    println!("-> Type e to exit");
+    println!("-> Type h for help");
+
+    loop{
+        println!("Current result: {}", result);
+        match get_input().unwrap() {
+            InputResult::Exit => exit_withMessage(),
+            InputResult::Up | InputResult::Right => {
+                println!("OK! Guessing up!");
+                result += (u32::MAX - result)/2;
+            },
+            InputResult::Down | InputResult::Right => {
+                println!("OK! Guessing down!");
+                result -= result/2;
+            },
+            _ => println!("Try again!"),
+        }
+
+    }
+
+    return;
+}
+
 fn get_input() -> io::Result<InputResult> {
     enable_raw_mode()?;
-    let result = InputResult::InvalidKey;
     loop {
         let event = event::read()?;
         let result = match  event{
                 Event::Key(KeyEvent {code: KeyCode::Char('e'), ..}) =>  InputResult::Exit,
                 Event::Key(KeyEvent {code: KeyCode::Char('h'), ..}) =>  InputResult::Help,
                 Event::Key(KeyEvent {code: KeyCode::Char(c), ..}) =>  InputResult::Char(c),
+                Event::Key(KeyEvent {code: KeyCode::Up, ..}) => InputResult::Up,
+                Event::Key(KeyEvent {code: KeyCode::Down, ..}) => InputResult::Down,
+                Event::Key(KeyEvent {code: KeyCode::Left, ..}) => InputResult::Left,
+                Event::Key(KeyEvent {code: KeyCode::Right, ..}) => InputResult::Right,
+                Event::Key(KeyEvent {code: KeyCode::Esc, ..}) => InputResult::Exit,
                 _ => InputResult::InvalidKey,            
         };
-        println!("You pressed: {:?}", event);
-        println!("\tClassified as: {:?}", result);
         if let InputResult::InvalidKey = result {
-            // If the key is invalid, continue to read the next event
             continue;
         }else {
-            break;
+            disable_raw_mode()?;
+            return Ok(result);
         }
     }
+}
 
-    disable_raw_mode()?;
-    
-    Ok(result)
+fn process_input() {
+
 }
 
 
-fn main() {
-    println!("{:?}", get_input().unwrap_or(InputResult::InvalidKey));
 
-
-    return;
-
-
-
-    println!("Hello, lets play a guessing game!");
-    println!("-> Press Enter to start");
-    println!("-> Type e to exit");
-    println!("-> Type h for help");
-
-    let mut input = String::new();
-    std::io::stdin().read_line(&mut input).expect("Failed to read line");
-    if input.trim() == "e" {
-        println!("Exiting the game. Goodbye!");
-        return;
-    } else if input.trim() == "h" {
-        println!("Help: Choose a number between 1 and 10 and try to guess it.");
-        return;
-    }
-
-    if input.trim().is_empty() {
-        println!("Great! Let's begin.");
-    } else {
-        println!("Invalid input. Please press Enter to start.");
-        return;
-    }
-    
-
-    println!("Choose a number between 1 and 10 but don't tell me!");
-    println!("I will try to guess it. You can type 'e' to exit at any time.");
-    println!("Press Enter to continue...");
-    input.clear();
-    std::io::stdin().read_line(&mut input).expect("Failed to read line");
-    if input.trim() == "e" {
-        println!("Exiting the game. Goodbye!");
-        return;
-    }
-    println!("Okay, I will start guessing now.");
-    // println!
+fn exit_withMessage() {
+    println!("\nExiting the game. Goodbye!");
+    std::process::exit(0);
+}
 
 
 
-    
+fn setup(default_press_handler: &mut HashMap<InputResult, Box<dyn Fn()>>) {
+    default_press_handler.insert(InputResult::Exit, Box::new(exit_withMessage));
+    default_press_handler.insert(InputResult::Help, Box::new(print_help));
+    default_press_handler.insert(InputResult::InvalidKey, Box::new( || {
+        println!("Invalid key pressed, please try again.");
+    }));
+    default_press_handler.insert(InputResult::Up, Box::new(|| {
+        increase_guess();
+    }));
+    default_press_handler.insert(InputResult::Down, Box::new(|| {
+        decrease_guess();
+    }));
+    default_press_handler.insert(InputResult::Left, Box::new( || {
+        decrease_guess();
+    }));
+    default_press_handler.insert(InputResult::Right, Box::new( || {
+        increase_guess();
+    }));
+    default_press_handler.insert(InputResult::Char(' '), Box::new( || {
+        println!("You pressed the space bar.");
+    }));
 
-    // ask for user input
+}
+
+
+
+fn increase_guess(){
+
+}
+fn decrease_guess(){
+
+}
+
+fn print_help() {
+    println!("\nHelp Menu:");
+    println!("- Press 'e' or Esc to exit the game.");
+    println!("- Press 'h' to display this help menu.");
+    println!("- Use arrow keys - up, right - to increase, down, left - to decrease the guess.");
 }
